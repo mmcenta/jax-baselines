f7013dc9b9a1a0dfa667eebc4b979bd173312dce
Matheus Centa <matheuscenta@gmail.com>
2020-05-18 20:58:07 +0200

    Implement Prioritezed Replay Buffer

--- jax_baselines/dqn/dqn.py
+++ jax_baselines/dqn/dqn.py
@@ -16,7 +16,7 @@ from jax_baselines.common.critic import DiscreteActionCritic
 from jax_baselines.common.learner import ActionCriticLearner
 from jax_baselines.common.scheduler import LinearDecay
 from jax_baselines.common.util import make_preprocessor
-from jax_baselines.dqn.replay import ReplayBuffer
+from jax_baselines.dqn.replay import ReplayBuffer, PrioritizedReplayBuffer
 from jax_baselines.save import load_from_zip, save_to_zip
 
 
@@ -24,6 +24,8 @@ def learn(rng, env_fn, net_fn,
           gamma=0.99, lr=5e-4, steps=1e6, batch_size=100, epsilon=0.1,
           epsilon_scheduler=None, warmup=1000, train_freq=1,
           buffer_size=50000, eval_freq=1e4, eval_episodes=10,
+          double_q=False, prioritized_replay=False, alpha=0.6,
+          beta=0.4, beta_scheduler=None,
           save_dir='./experiments/dqn', save_freq=1e4,
           logger_format_strs=None):
     """
@@ -36,9 +38,11 @@ def learn(rng, env_fn, net_fn,
     # get observation preprocessor
     preprocess = make_preprocessor()
 
-    # get epsilon scheduler
+    # get schedulers
     if epsilon_scheduler is None:
         epsilon_scheduler = lambda i: epsilon
+    if beta_scheduler is None:
+        beta_scheduler = lambda i: beta
 
     # make sure there are sufficient examples for a batch after warmup
     warmup = max(warmup, batch_size)
@@ -52,7 +56,11 @@ def learn(rng, env_fn, net_fn,
     # initialize replay buffer
     dummy_obs = preprocess(env.observation_space.sample())
     dummy_act = env.action_space.sample()
-    buffer = ReplayBuffer(buffer_size, dummy_obs, dummy_act)
+    if prioritized_replay:
+        buffer = PrioritizedReplayBuffer(buffer_size, dummy_obs, dummy_act,
+            alpha=alpha)
+    else:
+        buffer = ReplayBuffer(buffer_size, dummy_obs, dummy_act)
 
     # create action critic
     def loss_fn(target, q_val):
@@ -63,7 +71,7 @@ def learn(rng, env_fn, net_fn,
         adam(lr),
         loss_fn,
         target_train_steps=1000,
-        double_q=True,
+        double_q=double_q,
     )
 
     # initialize state
@@ -100,7 +108,7 @@ def learn(rng, env_fn, net_fn,
         # update the network
         if i >= warmup and (i - warmup) % train_freq == 0:
             j = (i - warmup) // train_freq # update step
-            batch = buffer.get_batch(batch_size)
+            batch = buffer.sample(batch_size)
 
             # update paramaters
             state = critic.update(j, state, batch)
--- jax_baselines/dqn/replay.py
+++ jax_baselines/dqn/replay.py
@@ -1,8 +1,12 @@
+# Adapted from OpenAI's implementation at https://github.com/openai/baselines
+import random
+
 import jax.numpy as jnp
 import numpy as np
 from jax.ops import index_update
 
 from jax_baselines.common.util import add_batch_dim
+from jax_baselines.common.segment_tree import MinSegmentTree, SumSegmentTree
 
 
 class ReplayBuffer:
@@ -15,36 +19,120 @@ class ReplayBuffer:
         self.obs = np.zeros(add_batch_dim(capacity, obs.shape))
         self.act = np.zeros(add_batch_dim(capacity, act.shape))
         self.rew = np.zeros((capacity,))
-        self.is_terminal = np.zeros((capacity,), dtype=bool)
         self.next_obs = np.zeros(add_batch_dim(capacity, obs.shape))
+        self.is_terminal = np.zeros((capacity,), dtype=bool)
 
-        self.ptr, self.size, self.capacity = 0, 0, capacity
+        self._ptr, self._size, self.capacity = 0, 0, capacity
 
-    def store(self, obs, act, rew,  next_obs, is_terminal=False):
-        self.obs[self.ptr] = obs
-        self.act[self.ptr] = act
-        self.rew[self.ptr] = rew
-        self.is_terminal[self.ptr] = is_terminal
-        self.next_obs[self.ptr] = next_obs
+    def __len__(self):
+        return self._size
 
-        self.ptr = (self.ptr + 1) % self.capacity
-        self.size = min(self.size + 1, self.capacity)
+    def store(self, obs, act, rew, next_obs, is_terminal=False):
+        self.obs[self._ptr] = obs
+        self.act[self._ptr] = act
+        self.rew[self._ptr] = rew
+        self.next_obs[self._ptr] = next_obs
+        self.is_terminal[self._ptr] = is_terminal
 
-    def get_batch(self, batch_size):
-        batch_indexes = np.random.choice(self.size, size=batch_size)
-        obs = self.obs[batch_indexes]
-        act = self.act[batch_indexes]
-        rew = self.rew[batch_indexes]
-        is_terminal = self.is_terminal[batch_indexes]
-        next_obs = self.next_obs[batch_indexes]
+        self._ptr = (self._ptr + 1) % self.capacity
+        self._size = min(self._size + 1, self.capacity)
+
+    def _get_batch_from_indexes(self, idxes):
+        obs = self.obs[idxes]
+        act = self.act[idxes]
+        rew = self.rew[idxes]
+        next_obs = self.next_obs[idxes]
+        is_terminal = self.is_terminal[idxes]
         return {
             "observations": jnp.device_put(obs),
             "actions": jnp.device_put(act),
             "rewards": jnp.device_put(rew),
-            "is_terminal": jnp.device_put(is_terminal),
             "next_observations": jnp.device_put(next_obs),
+            "is_terminal": jnp.device_put(is_terminal),
         }
 
+    def sample(self, batch_size):
+        batch_indexes = np.random.choice(self._size, size=batch_size)
+        return self._get_batch_from_indexes(batch_indexes)
 
-class PrioritizedReplayBuffer:
-    def __init__(self, capacity, obs, act):
+
+class PrioritizedReplayBuffer(ReplayBuffer):
+    def __init__(self, capacity, obs, act, alpha=0.6):
+        """
+        """
+        super(PrioritizedReplayBuffer, self).__init__(capacity, obs, act)
+
+        assert alpha >= 0
+        self.alpha = alpha
+
+        it_capacity = 1
+        while it_capacity < capacity:
+            it_capacity *= 2
+
+        self._it_sum = SumSegmentTree(it_capacity)
+        self._it_min = MinSegmentTree(it_capacity)
+        self._max_priority = 1.0
+
+    def store(self, *args, **kwargs):
+        """
+        """
+        idx = self._ptr
+        super().store(*args, **kwargs)
+        self._it_sum[idx] = self._max_priority ** self._alpha
+        self._it_min[idx] = self._max_priority ** self._alpha
+
+    def _sample_proportional(self, batch_size):
+        """
+        """
+        res = []
+        p_total = self._it_sum.sum(0, len(self._storage) - 1)
+        every_range_len = p_total / batch_size
+        for i in range(batch_size):
+            mass = random.random() * every_range_len + i * every_range_len
+            idx = self._it_sum.find_prefixsum_idx(mass)
+            res.append(idx)
+        return res
+
+    def sample(self, batch_size, beta):
+        """
+        """
+        assert beta > 0
+
+        idxes = self._sample_proportional(batch_size)
+
+        weights = []
+        p_min = self._it_min.min() / self._it_sum.sum()
+        max_weight = (p_min * len(self._storage)) ** (-beta)
+
+        for idx in idxes:
+            p_sample = self._it_sum[idx] / self._it_sum.sum()
+            weight = (p_sample * len(self._storage)) ** (-beta)
+            weights.append(weight / max_weight)
+        weights = np.array(weights)
+
+        batch = self._get_batch_from_indexes(idxes)
+        batch["is_weights"] = jnp.device_put(weights)
+        batch["indexes"] = idxes
+        return batch
+
+    def update_priorities(self, idxes, priorities):
+        """Update priorities of sampled transitions.
+        sets priority of transition at index idxes[i] in buffer
+        to priorities[i].
+        Parameters
+        ----------
+        idxes: [int]
+            List of idxes of sampled transitions
+        priorities: [float]
+            List of updated priorities corresponding to
+            transitions at the sampled idxes denoted by
+            variable `idxes`.
+        """
+        assert len(idxes) == len(priorities)
+        for idx, priority in zip(idxes, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self._storage)
+            self._it_sum[idx] = priority ** self._alpha
+            self._it_min[idx] = priority ** self._alpha
+
+            self._max_priority = max(self._max_priority, priority)
